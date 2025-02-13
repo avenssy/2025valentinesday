f60c34852b7d98fcd493d1f7b26cd189eabf0ff1
Aven <avenssy@gmail.com>
2025-02-13 17:08:11 +0800

    apply git pages changes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export", // Required for GitHub Pages (static export)
+  images: {
+    unoptimized: true, // Disable Next.js image optimization (GitHub Pages does not support it)
+  },
+  basePath: "/2025valentinesday", // Change this to your GitHub repository name
+  assetPrefix: "/your-repo-name/",
 };
 
 export default nextConfig;
